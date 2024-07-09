72d2ce155d68c917245824233f4939376316866f
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-07-08 21:13:28 -0600

    added icons and final touch ups to dark mode

--- assets/logic.js
+++ assets/logic.js
@@ -1,12 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.getElementById('mode-toggle');
 
+    const updateToggleButtonIcon = () => {
+        if(document.body.classList.contains('dark-mode')) {
+            toggleButton.textContent = '🌙';
+        } else {
+            toggleButton.textContent = '☀️';
+        }
+    };
+
     if (localStorage.getItem('dark-mode') === 'enabled') {
         document.body.classList.add('dark-mode');
+        updateToggleButtonIcon();
     }
 
     toggleButton.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
+        updateToggleButtonIcon();
 
         if (document.body.classList.contains('dark-mode')) {
             localStorage.setItem('dark-mode', 'enabled');
