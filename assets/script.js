62d86a53b976b9dc5e98cbb44080cc9e7d173f77
zhill0950 <zhill0950@gmail.com>
2024-07-09 19:23:37 -0700

    merge conflicts

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,8 @@ updateCardStorage();
 const submit = document.getElementById("submit");
 submit.addEventListener("click", addArticle);
 
-// Creates a new task object with Title, Description, Due-Date and Priority. Adds the object to storage and calls a function to create the new task.
+// Creates a new task object with Title, Description, Due-Date and Priority.
+// Adds the object to storage and calls a function to create the new task.
 function addArticle() {
   const newCard = {
     Title: formTitle.value,
@@ -36,22 +37,31 @@ function addArticle() {
 function renderStoredTaskCards() {
   for (let i = 0; i < storedTaskCards.length; i++) {
     const article = document.createElement("article");
+    article.setAttribute("data-index", i);
 
     const title = document.createElement("h2");
     const description = document.createElement("h3");
     const duedate = document.createElement("p");
-    const priority = document.createElement("p");
+    const priority = document.createElement("button");
+    const priorityDiv = document.createElement("div");
+    const deleteBtn = document.createElement("button");
+
+    deleteBtn.setAttribute("class", "delete is-pulled-right");
+    priorityDiv.setAttribute("class", "priority-div");
+    priority.setAttribute("class", "button is-danger is-small");
 
     title.textContent = storedTaskCards[i].Title;
     description.textContent = storedTaskCards[i].Description;
     duedate.textContent = "Due: " + storedTaskCards[i]["Due Date"];
     priority.textContent = "Priority: " + storedTaskCards[i].Priority;
 
+    article.appendChild(deleteBtn);
     article.appendChild(title);
     article.appendChild(duedate);
-    article.appendChild(priority);
     article.appendChild(description);
-    formContainer.appendChild(article); 
+    article.appendChild(priorityDiv);
+    priorityDiv.appendChild(priority);
+    formContainer.appendChild(article);
   }
 }
 
@@ -63,21 +73,33 @@ function createTaskCards() {
   const description = document.createElement("h3");
   const duedate = document.createElement("p");
   const priority = document.createElement("p");
+  const priorityDiv = document.createElement("div");
+  const deleteBtn = document.createElement("button");
+
+  priority.setAttribute("class", "button is-danger is-small");
+  priorityDiv.setAttribute("class", "priority-div");
+  deleteBtn.setAttribute("class", "delete is-pulled-right");
 
   title.textContent = formTitle.value;
   description.textContent = formDescription.value;
   duedate.textContent = "Due: " + formDueDate.value;
   priority.textContent = "Priority: " + formPriority.value;
 
+  article.appendChild(deleteBtn);
   article.appendChild(title);
   article.appendChild(duedate);
-  article.appendChild(priority);
   article.appendChild(description);
+  article.appendChild(priorityDiv);
+  priorityDiv.appendChild(priority);
   formContainer.appendChild(article);
+
+  formTitle.value = "";
+  formDescription.value = "";
+  formDueDate.value = "";
 }
 
 // Gets the storedTaskCards object array in local storage, if there is one.
-function updateCardStorage(){
+function updateCardStorage() {
   const storedTasks = getLocalStorageObj("Task Cards");
   if (storedTasks !== null) {
     storedTaskCards = storedTasks;
