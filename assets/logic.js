77ca96d70de488ac3bd32b29eabdc0e06daba229
zhill0950 <zhill0950@gmail.com>
2024-07-11 09:31:37 -0700

    fix color discrepancies on logic.js

--- assets/logic.js
+++ assets/logic.js
@@ -4,69 +4,71 @@ const leftContainerDueDate = document.querySelector("#form-duedate");
 const leftContainerPriority = document.querySelector("#form-priority");
 const leftContainerSubmitBnt = document.querySelector("#submit");
 const leftContainerClearAllBtn = document.querySelector("#clearBtn");
+const leftContainerAboutBtn = document.getElementById("aboutBtn");
 
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.getElementById("mode-toggle");
+  const logoImg = document.getElementById("mode-img-toggle");
 
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.getElementById('mode-toggle');
-    const logoImg = document.getElementById('mode-img-toggle');
-
-    const updateToggleButtonIcon = () => {
-        if(document.body.classList.contains('dark-mode')) {
-            toggleButton.textContent = '🌙';
-        } else {
-            toggleButton.textContent = '☀️';
-        }
-    };
-
-    const toggleLogoImg = () => {
-        if(document.body.classList.contains('dark-mode')) {
-            logoImg.src = "./assets/images/projectpalheader-dark.png";
-        } else {
-            logoImg.src = "./assets/images/projectpalheader.png";
-        }
+  const updateToggleButtonIcon = () => {
+    if (document.body.classList.contains("dark-mode")) {
+      toggleButton.textContent = "🌙";
+    } else {
+      toggleButton.textContent = "☀️";
     }
+  };
 
-    
-
-    if (localStorage.getItem('dark-mode') === 'enabled') {
-        document.body.classList.add('dark-mode');
-        leftContainerTitle.classList.add('has-background-grey-darker');
-        leftContainerTitle.classList.add('has-text-white');
-        leftContainerDescription.classList.add('has-background-grey-darker');
-        leftContainerDescription.classList.add('has-text-white');
-        leftContainerDueDate.classList.add('has-background-grey-darker');
-        leftContainerDueDate.classList.add('has-text-white');
-        leftContainerPriority.classList.add('has-background-grey-darker');
-        leftContainerPriority.classList.add('has-text-white');
-        leftContainerSubmitBnt.classList.add('has-background-grey-darker');
-        leftContainerSubmitBnt.classList.add('has-text-white');
-        leftContainerClearAllBtn.classList.add('has-background-grey-darker');
-        leftContainerClearAllBtn.classList.add('has-text-white');
-        updateToggleButtonIcon();
-        toggleLogoImg();
+  const toggleLogoImg = () => {
+    if (document.body.classList.contains("dark-mode")) {
+      logoImg.src = "./assets/images/projectpalheader-dark.png";
+    } else {
+      logoImg.src = "./assets/images/projectpalheader.png";
     }
+  };
 
-    toggleButton.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
-        leftContainerTitle.classList.toggle('has-background-grey-darker');
-        leftContainerTitle.classList.toggle('has-text-white');
-        leftContainerDescription.classList.toggle('has-background-grey-darker');
-        leftContainerDescription.classList.toggle('has-text-white');
-        leftContainerDueDate.classList.toggle('has-background-grey-darker');
-        leftContainerDueDate.classList.toggle('has-text-white');
-        leftContainerPriority.classList.toggle('has-background-grey-darker');
-        leftContainerPriority.classList.toggle('has-text-white');
-        leftContainerSubmitBnt.classList.toggle('has-background-grey-darker');
-        leftContainerSubmitBnt.classList.toggle('has-text-white');
-        leftContainerClearAllBtn.classList.toggle('has-background-grey-darker');
-        leftContainerClearAllBtn.classList.toggle('has-text-white');
-        updateToggleButtonIcon();
-        toggleLogoImg();
+  if (localStorage.getItem("dark-mode") === "enabled") {
+    document.body.classList.add("dark-mode");
+    leftContainerTitle.classList.add("has-background-grey-darker");
+    leftContainerTitle.classList.add("has-text-grey");
+    leftContainerDescription.classList.add("has-background-grey-darker");
+    leftContainerDescription.classList.add("has-text-grey");
+    leftContainerDueDate.classList.add("has-background-grey-darker");
+    leftContainerDueDate.classList.add("has-text-grey");
+    leftContainerPriority.classList.add("has-background-grey-darker");
+    leftContainerPriority.classList.add("has-text-grey");
+    leftContainerSubmitBnt.classList.add("has-background-grey-darker");
+    leftContainerSubmitBnt.classList.add("has-text-grey");
+    leftContainerClearAllBtn.classList.add("has-background-grey-darker");
+    leftContainerClearAllBtn.classList.add("has-text-grey");
+    leftContainerAboutBtn.classList.add("has-background-grey-darker");
+    leftContainerAboutBtn.classList.add("has-text-grey");
+    updateToggleButtonIcon();
+    toggleLogoImg();
+  }
 
-        if (document.body.classList.contains('dark-mode')) {
-            localStorage.setItem('dark-mode', 'enabled');
-        } else {
-            localStorage.setItem('dark-mode', 'disabled');
-        }
-    });
+  toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    leftContainerTitle.classList.toggle("has-background-grey-darker");
+    leftContainerTitle.classList.toggle("has-text-grey");
+    leftContainerDescription.classList.toggle("has-background-grey-darker");
+    leftContainerDescription.classList.toggle("has-text-grey");
+    leftContainerDueDate.classList.toggle("has-background-grey-darker");
+    leftContainerDueDate.classList.toggle("has-text-grey");
+    leftContainerPriority.classList.toggle("has-background-grey-darker");
+    leftContainerPriority.classList.toggle("has-text-grey");
+    leftContainerSubmitBnt.classList.toggle("has-background-grey-darker");
+    leftContainerSubmitBnt.classList.toggle("has-text-grey");
+    leftContainerClearAllBtn.classList.toggle("has-background-grey-darker");
+    leftContainerClearAllBtn.classList.toggle("has-text-grey");
+    leftContainerAboutBtn.classList.toggle("has-background-grey-darker");
+    leftContainerAboutBtn.classList.toggle("has-text-grey");
+    updateToggleButtonIcon();
+    toggleLogoImg();
+
+    if (document.body.classList.contains("dark-mode")) {
+      localStorage.setItem("dark-mode", "enabled");
+    } else {
+      localStorage.setItem("dark-mode", "disabled");
+    }
+  });
 });
