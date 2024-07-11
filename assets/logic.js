9e45d655f59b8ac806f25b0aff10f7d9b9d6e7af
Cameron Barfuss <128769599+Runnerrupert@users.noreply.github.com>
2024-07-11 16:27:28 -0600

    Feature - Modal Dark-Mode (#19)
    * Add object function and class selectors
    
    * test commit
    
    * Add local storage functions
    
    * Add functions to render tasks on page load, and when submit button is pressed
    
    * Add comments to explain code
    
    * Fixed changed id's
    
    * Add new dark-mode logo img, update logic.js to change logo based on light/dark mode
    
    * Create Clear All button, fix header with logo and dark button
    
    * Add dark-mode triggers for the left container, adjust some positioning in the CSS
    
    * Add about modal and adjust the left container's css
    
    * Add dark-mode for the modal
    
    * Clean logic.js code by making an array with all querySelectors
    
    * Add Comments to code for logic.js
    
    * Add Comments to code for script.js
    
    * Add Comments to code in styles.css and index.html
    
    * Add sorting to the tasks based on Priority

--- assets/logic.js
+++ assets/logic.js
@@ -1,15 +1,32 @@
+// Element Selectors by ID
+const projectBody = document.querySelector("#project-body");
 const leftContainerTitle = document.querySelector("#form-title");
 const leftContainerDescription = document.querySelector("#form-description");
 const leftContainerDueDate = document.querySelector("#form-duedate");
 const leftContainerPriority = document.querySelector("#form-priority");
 const leftContainerSubmitBnt = document.querySelector("#submit");
 const leftContainerClearAllBtn = document.querySelector("#clearBtn");
-const leftContainerAboutBtn = document.getElementById("aboutBtn");
+const leftContainerAboutBtn = document.querySelector("#aboutBtn");
+const modalHeader = document.querySelector("#modal-header");
+const modalBody = document.querySelector("#modal-body");
+const modalTitle = document.querySelector("#modal-title");
+const modalFirstQuestion = document.querySelector("#modal-question1");
+const modalSecondQuestion = document.querySelector("#modal-question2");
+const modalThirdQuestion = document.querySelector("#modal-question3");
 
+// Array containing all selectors with the exception of the projectBody
+let darkModeArray = [leftContainerTitle, leftContainerDescription, leftContainerDueDate, leftContainerPriority, 
+    leftContainerSubmitBnt, leftContainerClearAllBtn, leftContainerAboutBtn, modalHeader, modalBody, 
+    modalTitle, modalFirstQuestion, modalSecondQuestion, modalThirdQuestion];
+
+// Event Listener for initializing light/dark-mode when the page is loaded
 document.addEventListener("DOMContentLoaded", () => {
+    
+    // ID selectors for changing the logo and light/dark button
   const toggleButton = document.getElementById("mode-toggle");
   const logoImg = document.getElementById("mode-img-toggle");
 
+    // Updates the light/dark button icon based on which mode the user is in
   const updateToggleButtonIcon = () => {
     if (document.body.classList.contains("dark-mode")) {
       toggleButton.textContent = "🌙";
@@ -18,6 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
+    // Updates the projectPal logo based on which mode the user is in
   const toggleLogoImg = () => {
     if (document.body.classList.contains("dark-mode")) {
       logoImg.src = "./assets/images/projectpalheader-dark.png";
@@ -26,45 +44,32 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
+    // Updates all elements by adding specified classes for dark-mode
   if (localStorage.getItem("dark-mode") === "enabled") {
-    document.body.classList.add("dark-mode");
-    leftContainerTitle.classList.add("has-background-grey-darker");
-    leftContainerTitle.classList.add("has-text-grey");
-    leftContainerDescription.classList.add("has-background-grey-darker");
-    leftContainerDescription.classList.add("has-text-grey");
-    leftContainerDueDate.classList.add("has-background-grey-darker");
-    leftContainerDueDate.classList.add("has-text-grey");
-    leftContainerPriority.classList.add("has-background-grey-darker");
-    leftContainerPriority.classList.add("has-text-grey");
-    leftContainerSubmitBnt.classList.add("has-background-grey-darker");
-    leftContainerSubmitBnt.classList.add("has-text-grey");
-    leftContainerClearAllBtn.classList.add("has-background-grey-darker");
-    leftContainerClearAllBtn.classList.add("has-text-grey");
-    leftContainerAboutBtn.classList.add("has-background-grey-darker");
-    leftContainerAboutBtn.classList.add("has-text-grey");
+    projectBody.classList.add("dark-mode");
+    
+    for (let i = 0; i < darkModeArray.length; i++) {
+        darkModeArray[i].classList.add("has-background-grey-darker");
+        darkModeArray[i].classList.add("has-text-grey");
+    }
+
     updateToggleButtonIcon();
     toggleLogoImg();
   }
 
+    // Updates all elements by toggling-on/off specified classes for dark-mode when the button is pressed
   toggleButton.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    leftContainerTitle.classList.toggle("has-background-grey-darker");
-    leftContainerTitle.classList.toggle("has-text-grey");
-    leftContainerDescription.classList.toggle("has-background-grey-darker");
-    leftContainerDescription.classList.toggle("has-text-grey");
-    leftContainerDueDate.classList.toggle("has-background-grey-darker");
-    leftContainerDueDate.classList.toggle("has-text-grey");
-    leftContainerPriority.classList.toggle("has-background-grey-darker");
-    leftContainerPriority.classList.toggle("has-text-grey");
-    leftContainerSubmitBnt.classList.toggle("has-background-grey-darker");
-    leftContainerSubmitBnt.classList.toggle("has-text-grey");
-    leftContainerClearAllBtn.classList.toggle("has-background-grey-darker");
-    leftContainerClearAllBtn.classList.toggle("has-text-grey");
-    leftContainerAboutBtn.classList.toggle("has-background-grey-darker");
-    leftContainerAboutBtn.classList.toggle("has-text-grey");
+    projectBody.classList.toggle("dark-mode");
+
+    for (i = 0; i < darkModeArray.length; i++) {
+        darkModeArray[i].classList.toggle("has-background-grey-darker");
+        darkModeArray[i].classList.toggle("has-text-grey");
+    }
+
     updateToggleButtonIcon();
     toggleLogoImg();
 
+        // Saves the users current mode in local storage, Enabled = Dark-Mode ; Disabled = Light-Mode
     if (document.body.classList.contains("dark-mode")) {
       localStorage.setItem("dark-mode", "enabled");
     } else {
